e329b318b47abcdcf761bc744f6fd141793dc70d
AlgoTeche <tech.vitthal@gmail.com>
2026-05-09 14:37:21 +0530

    validation added to dto

--- student-management-system/src/main/java/io/algotechie/sms/dto/StudentDto.java
+++ student-management-system/src/main/java/io/algotechie/sms/dto/StudentDto.java
@@ -1,6 +1,8 @@
 package io.algotechie.sms.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +14,11 @@ import lombok.Setter;
 @AllArgsConstructor
 public class StudentDto {
     private Long id;
+    @NotEmpty(message = "Student First Name should not empty, Please Enter first Name")
     private String firstName;
+    @NotEmpty(message = "Student last name should not empty, please enter last name")
     private String lastName;
+    @NotEmpty(message = "Student email should not empty, please enter valid email address")
+    @Email
     private String email;
 }
